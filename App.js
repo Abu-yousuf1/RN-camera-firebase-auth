a6059525ef9ac49f2c9c5ae4178cd208326f3434
YousufAdnan- <yousuf.adnan@inmogr.com>
2022-02-28 21:11:17 +0600

    camera integration done

--- App.js
+++ App.js
@@ -1,38 +1,22 @@
-import { View, Text,Button } from 'react-native'
 import React from 'react'
-import auth from '@react-native-firebase/auth';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import GoogleSign from "./Components/GoogleSign"
+import Camera from './Components/Camera'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 export default function App() {
-  GoogleSignin.configure({
-    webClientId: '231493047400-lul43ei0bm8s1a5gtib0rbmr6bvh2mbn.apps.googleusercontent.com',
-  });
-
-  const signInWithGoogleAsync=async()=>{
-        // Get the users ID token
-        const { idToken } = await GoogleSignin.signIn();
-
-        // Create a Google credential with the token
-        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-
-        // Sign-in the user with the credential
-        const user_sign_in= auth().signInWithCredential(googleCredential);
-
-        user_sign_in.then((user)=>{
-          console.log(user);
-        })
-        .catch((error)=>{
-          console.log(error)
-        })
-  }
+  const Stack = createNativeStackNavigator();
 
   return (
-    <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-     <Button
-     title="Sign in with Google"
-     onPress={signInWithGoogleAsync}
-     />
-    </View>
+    // <View>
+    //   <Camera/>
+    // </View>
+    <NavigationContainer>
+       <Stack.Navigator initialRouteName="Home">
+           <Stack.Screen name="Home" component={GoogleSign}/>
+            <Stack.Screen name="camera" component={Camera} /> 
+    </Stack.Navigator>
+    </NavigationContainer>
   )
 }
